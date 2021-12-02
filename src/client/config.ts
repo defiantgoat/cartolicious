1fbfccf8b1019b9d5adae9ab25d2466b11d1c3c2
Jayson Ward <defiantgoat@gmail.com>
2021-12-02 16:32:38 +1100

    Added google analytics, update to https

--- src/client/config.ts
+++ src/client/config.ts
@@ -2,7 +2,7 @@ export const APP_NAME = "Cartolicious";
 export const VERSION = "1.0.0";
 
 export const MAP_CONFIG = {
-  MIN_ZOOM: 9,
+  MIN_ZOOM: 2,
   MAX_ZOOM: 16,
   DEFAULT_ZOOM: 12,
   DEFAULT_CENTER: [-8573959.232998796, 4705918.436640128],
@@ -10,5 +10,5 @@ export const MAP_CONFIG = {
 
 export const ENDPOINTS = {
   GET_STYLES: (token: string) =>
-    `http://api.cartolicious.com/v1/styles?token=${token}`,
+    `https://api.cartolicious.com/v1/styles?token=${token}`,
 };
